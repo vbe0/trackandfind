d3769d1fd9675e03ebff8005cdc4370e30d094c4
Valter Berg <vbe013@post.uit.no>
2018-03-18 13:04:53 +0100

    test

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ var tools = require('./data/sensordata.js');
 
 var app = express();
 
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 //var io = require('socket.io')(3000);
 var port = process.env.PORT || 8080;
-
 
 
 // ================================================================
