00f46e7785816e40f5df06647becfc8d7945783e
Valter Berg <vbe013@post.uit.no>
2018-03-18 09:03:53 +0100

    test

--- data/sensordata.js
+++ data/sensordata.js
@@ -29,9 +29,8 @@ module.exports =
         s = JSON.parse(payload.toString())['state']['reported']['payload'];
         console.log('Message: ', s)
         console.log("GG: ", payload.toString())
-
         // Broadcast the message to any connected socket clients
-        io.emit('broadcast', {topic, message: payload.toString()});
+        // io.emit('broadcast', {topic, message: payload.toString()});
         return s;
         
         });
