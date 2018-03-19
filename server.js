a4de3deccce0403a4e583fd8d634e1f17ba3e4b9
Valter Berg <vbe013@post.uit.no>
2018-03-19 11:58:07 +0100

    fixed popup med tid

--- server.js
+++ server.js
@@ -15,49 +15,17 @@ var io = require('socket.io')(server);
 io = require('socket.io')(3000);
 var port = process.env.PORT || 8080;
 
+var favicon = require('serve-favicon')
+var path = require('path')
+
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
+
 
 // ================================================================
 // setup our express application
 // ================================================================
 app.use('/public', express.static(process.cwd() + '/public'));
 app.set('view engine', 'ejs');
-
-
-
-var awsIot = require('aws-iot-device-sdk');
-
-var thingName = '00001319'; // Replace with your own thing name
-
-var fs = require("fs");
-
-var device = awsIot.device({
-   keyPath: './certs/privkey.pem',
-  certPath: './certs/cert.pem',
-    caPath: './certs/ca.pem',
-  clientId: thingName,
-      host: 'a31ovqfkmg1ev8.iot.eu-west-1.amazonaws.com'
-});
-
-
-
-// When the MQTT client connects, subscribe to the thing topic
-device.on('connect', function() {
-console.log('Client connected');
-device.subscribe('$aws/things/' + thingName + '/shadow/update');
-});
-
-device.on('message', function(topic, payload) {
-//console.log('Message: ', topic, payload.toString());
-var s = JSON.parse(payload.toString())['state']['reported']['payload'];
-console.log('Message: ', s)
-console.log("GG: ", payload.toString())
-// Broadcast the message to any connected socket clients
-io.emit('broadcast', {topic, message: payload.toString()});
-return s;
-
-});
-
-
 
 // ================================================================
 // setup routes
@@ -71,4 +39,4 @@ app.listen(port, function() {
     console.log('Server listening on port ' + port + '...');
 });
 
-//tools.getSensorData(io);
+tools.getSensorData(io);
