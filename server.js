5bb0d9f60db3c8c7be09f801a00acf0984a9a902
Thomas Bye Nilsen <tni068@post.uit.no>
2018-03-19 10:44:30 +0100

    Will add datepickers now

--- server.js
+++ server.js
@@ -40,25 +40,6 @@ var device = awsIot.device({
 
 
 
-// When the MQTT client connects, subscribe to the thing topic
-device.on('connect', function() {
-console.log('Client connected');
-device.subscribe('$aws/things/' + thingName + '/shadow/update');
-});
-
-device.on('message', function(topic, payload) {
-//console.log('Message: ', topic, payload.toString());
-var s = JSON.parse(payload.toString())['state']['reported']['payload'];
-console.log('Message: ', s)
-console.log("GG: ", payload.toString())
-// Broadcast the message to any connected socket clients
-io.emit('broadcast', {topic, message: payload.toString()});
-return s;
-
-});
-
-
-
 // ================================================================
 // setup routes
 // ================================================================
