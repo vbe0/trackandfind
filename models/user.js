f34f64d3f5b5fec431b6caa81f3856c51bf7c7f5
Valter Berg <vbe013@post.uit.no>
2018-04-15 17:49:24 +0200

    fjerna bcrypt midlertidig for å få det til å funke

--- models/user.js
+++ models/user.js
@@ -1,5 +1,4 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
 
 var UserSchema = new mongoose.Schema({
   username: {
@@ -29,26 +28,32 @@ UserSchema.statics.authenticate = function (username, password, callback) {
         err.status = 401;
         return callback(err);
       }
-      bcrypt.compare(password, user.password, function (err, result) {
-        if (result === true) {
-          return callback(null, user);
-        } else {
-          return callback();
-        }
-      })
+      // bcrypt.compare(password, user.password, function (err, result) {
+      //   if (result === true) {
+      //     return callback(null, user);
+      //   } else {
+      //     return callback();
+      //   }
+      // })
+      if (password == user.password) {
+        return callback(null, user);
+      }
+      else {
+        return callback()
+      }
     });
 }
 
 //hashing a password before saving it to the database
 UserSchema.pre('save', function (next) {
   var user = this;
-  bcrypt.hash(user.password, 10, function (err, hash) {
-    if (err) {
-      return next(err);
-    }
-    user.password = hash;
-    next();
-  })
+  // bcrypt.hash(user.password, 10, function (err, hash) {
+  //   if (err) {
+  //     return next(err);
+  //   }
+  //   user.password = hash;
+  next();
+  // })
 });
 
 
