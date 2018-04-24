bedd8ee4bb4c00466eef4450d05f02b42c9052a1
Valter Berg <vbe013@post.uit.no>
2018-04-24 19:11:19 +0200

    Starta med cognito, Pontus sendte eksempel på mail. Dust prosjekt eksempel på pontus sin github

--- data/cognito.js
+++ data/cognito.js
@@ -1,67 +1,74 @@
 'use strict'
-var AWS = require('aws-sdk');
 const axios = require('axios')
+const Promise = require('bluebird')
 
+var AWS = require('aws-sdk');
 
 var url = 'https://1u31fuekv5.execute-api.eu-west-1.amazonaws.com/prod/manifest/?hostname=startiot.mic.telenorconnexion.com'
+var MANIFEST = {}
+
 
 const getManifest = async url => {
     try {
       const response = await axios.get(url);
       const data = response.data;
-      return data
+      MANIFEST = data
     } catch (error) {
       console.log(error);
     }
-  };
-function setUpLamda(){
-    return getManifest(url)
-    .then (manifest => {
+};
 
-        // Initialize Cognito by setting the Identity pool from the manifest.
-        // This will give us an "unauthenticated" role needed to invoke login.
-        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-            IdentityPoolId: manifest.IdentityPool
-        });
-        AWS.config.credentials.clearCachedId();
-        AWS.config.update({region:manifest.Region});
-        // Payload to the Auth API
-        var loginPayload = {
-            action: 'LOGIN',
-            attributes: {
-                userName: 'vbe013',
-                password: 'GGwpGGwp4567'
-            }
-        };
-        
-        // Provide the Auth Lambda function ID and the login payload
-        var params = {
-            FunctionName: manifest.AuthLambda,
-            Payload: JSON.stringify(loginPayload)
-        };
-        
-        // Invoke the Lambda
-        var lambda = new AWS.Lambda();
-        lambda.invoke(params, function(err, data) {
-            if (err) {
-                console.log("manifest: ", manifest.IdentityPool)
-                console.log(err, err.stack); // An error occurred
-            } else {
-                var result = JSON.parse(data.Payload);
-                
-                // Obtain a Cognito Identy with the returned credentials
-                var creds = result.credentials;
+async function setUpLamda(){
+    // Initialize Cognito by setting the Identity pool from the MANIFEST.
+    // This will give us an "unauthenticated" role needed to invoke login.
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: MANIFEST.IdentityPool
+    });
+    AWS.config.credentials.clearCachedId();
+    AWS.config.update({region:MANIFEST.Region});
+    // Payload to the Auth API
+    var loginPayload = {
+        action: 'LOGIN',
+        attributes: {
+            userName: 'vbe013',
+            password: 'GGwpGGwp4567'
+        }
+    };
+    
+    // Provide the Auth Lambda function ID and the login payload
+    var params = {
+        FunctionName: MANIFEST.AuthLambda,
+        Payload: JSON.stringify(loginPayload)
+    };
+    
+    // Invoke the Lambda
+    var lambda = new AWS.Lambda();
+    
+    await lambda.invoke(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack); // An error occurred
+        } else {
+            var result = JSON.parse(data.Payload);
+            
+            // Obtain a Cognito Identy with the returned credentials
+            var creds = result.credentials;
+            try{
                 AWS.config.credentials.params.Logins = {
-                    ['cognito-idp.' + manifest.Region + '.amazonaws.com/' + manifest.UserPool]: creds.token
+                    ['cognito-idp.' + MANIFEST.Region + '.amazonaws.com/' + MANIFEST.UserPool]: creds.token
                 };
-                AWS.config.credentials.expired = true;
+            } catch (err) {
+                return "failed"
             }
-        });
-        return lambda
-    })
+                AWS.config.credentials.expired = true;
+                //console.log("lamda: \n\n", lambda)
+            }
+        }).promise()
+
+    return lambda
 }
 
-
-setUpLamda().then( lambda => {
-    console.log(lambda)
+getManifest(url).then(r => {
+    setUpLamda().then(lambda => {
+        console.log(lambda)
+    })
 })
