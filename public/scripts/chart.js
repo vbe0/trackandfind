49d83c09c66a02a747e7c5d59ce978c8fc5c7a66
Thomas Bye Nilsen <tni068@post.uit.no>
2018-05-31 03:59:22 +0200

    accelerometer chart done

---
+++ public/scripts/chart.js
@@ -0,0 +1,33 @@
+
+
+function newChart(id) {
+    var options = {
+        height: '200px',
+        width: '1600px',
+        showGrid: false,
+        axisX: {
+            labelFontSize: 30
+        },
+        axisy: {
+            labelFontSize: 30
+        }
+    }
+
+    var data = {}
+    return new Chartist.Line("#theChart", data, options);
+}
+
+
+function setChartData(chart, data, idx, granularity) {
+    var labels = [], accs = [], i
+    for (var i = idx; i < Number(idx + Number(granularity)); i++) {
+        if (data[i].sumAcc != undefined) {
+            accs.push(Number(data[i].sumAcc))
+            labels.push([new Date(Number(data[i].date))])
+        } else {
+            return
+        }
+    }
+
+    chart.update({labels: labels, series: [accs]})
+}
